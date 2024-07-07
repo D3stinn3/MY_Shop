ddfca4895b3d63c0e9088883579e5481cf5c14c1
D3stinn3 <paynedestinne@gmail.com>
2024-07-07 18:53:20 +0300

    Settings

--- api_ecommerce/api_ecommerce/settings.py
+++ api_ecommerce/api_ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bn^sofm=v+l%$h47f80wbw^+6b5wt*n$)k1v!b$$52m&#l1$wm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
